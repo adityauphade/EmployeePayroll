61886d324584df45a4b2cf4873fe4d5f02326d90
Aditya Uphade <aditya.uphade@morningstar.com>
2021-08-24 01:16:49 +0530

    Introduced JSON to create an array of object to store in the HTML Local storage, Accessing them to push them into the Table

--- js/table.js
+++ js/table.js
@@ -1,87 +1,48 @@
-let tablecontent = `<thead>
-<tr>
-    <th></th>
-    <th>Name</th>
-    <th>Gender</th>
-    <th>Department</th>
-    <th>Salary</th>
-    <th>Start Date</th>
-    <th>Actions</th>
-</tr>
-</thead>
-<tbody>
-<tr>
-    <td class="e-profile">
-        <img src="../assets/Profiles/Ellipse -2.png" alt="User Profile">
-    </td>
-    <td class="e-name">
-        Aditya Uphade
-    </td>
-    <td class="e-gender">
-        Male
-    </td>
-    <td class="e-department">
-        Sales, Finance
-    </td>
-    <td class="e-salary">
-        Rs. 10,000
-    </td>
-    <td class="e-start-date">
-        29 Oct 2019
-    </td>
-    <td class="payroll-table-actions">
-        <img src="/assets/Logo/delete.jpg" alt="Delete">
-        <img src="/assets/Logo/edit.png" alt="Edit"> 
-    </td>
-</tr>
-<tr>
-    <td class="e-profile">
-        <img src="../assets/Profiles/Ellipse -2.png" alt="User Profile">
-    </td>
-    <td class="e-name">
-        Aditya Uphade
-    </td>
-    <td class="e-gender">
-        Male
-    </td>
-    <td class="e-department">
-        Sales, Finance
-    </td>
-    <td class="e-salary">
-        Rs. 10,000
-    </td>
-    <td class="e-start-date">
-        29 Oct 2019
-    </td>
-    <td class="payroll-table-actions">
-        <img src="/assets/Logo/delete.jpg" alt="Delete">
-        <img src="/assets/Logo/edit.png" alt="Edit"> 
-    </td>
-</tr>
-<tr>
-    <td class="e-profile">
-        <img src="../assets/Profiles/Ellipse -2.png" alt="User Profile">
-    </td>
-    <td class="e-name">
-        Aditya Uphade
-    </td>
-    <td class="e-gender">
-        Male
-    </td>
-    <td class="e-department">
-        Sales, Finance
-    </td>
-    <td class="e-salary">
-        Rs. 10,000
-    </td>
-    <td class="e-start-date">
-        29 Oct 2019
-    </td>
-    <td class="payroll-table-actions">
-        <img src="/assets/Logo/delete.jpg" alt="Delete">
-        <img src="/assets/Logo/edit.png" alt="Edit"> 
-    </td>
-</tr>
-</tbody>`
 
-document.getElementById("payroll-table").innerHTML = tablecontent;
+let employeeObjList = JSON.parse(localStorage.getItem(`EmployeePayrollList`));
+document.getElementById(`count`).innerText = employeeObjList.length;
+createTable();
+function createTable(){
+    let table_header = `<tr>
+        <th></th>
+        <th>Name</th>
+        <th>Gender</th>
+        <th>Department</th>
+        <th>Salary</th>
+        <th>Start Date</th>
+        <th>Actions</th>
+    </tr>`
+
+    let table_content = `${table_header}`;
+
+    employeeObjList.forEach(obj => {
+        table_content =`${table_content}
+        <tr>
+            <td class="e-profile">
+                <img src="../assets/Profiles/Ellipse -2.png" alt="User Profile">
+            </td>
+            <td class="e-name">
+                ${obj.name}
+            </td>
+            <td class="e-gender">
+                ${obj.gender}
+            </td>
+            <td class="e-department">
+                ${obj.department}
+            </td>
+            <td class="e-salary">
+                Rs. 10,000
+            </td>
+            <td class="e-start-date">
+                29 Oct 2019
+            </td>
+            <td class="payroll-table-actions">
+                <img src="/assets/Logo/delete.jpg" alt="Delete">
+                <img src="/assets/Logo/edit.png" alt="Edit"> 
+            </td>
+        </tr>`
+    });
+
+    console.log(table_content);
+    document.getElementById(`payroll-table`).innerHTML = table_content;
+}
